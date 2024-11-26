bd0399ded4927fc7d67fc6ebb90b4ec236434aae
kirill2ll <kirill2ll@mail.ru>
2024-11-26 18:53:03 +0200

    Add Redirect page for articles

--- first_site/my_app/urls.py
+++ first_site/my_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 #my_domain.com/my_app/simple_view
 urlpatterns = [
+    path('<int:num_page>/',views.num_page_view),
     path('<str:topic>/',views.news_view),
     path('<int:num1>/<int:num2>',views.add_view),
 
--- first_site/my_app/views.py
+++ first_site/my_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 articles = {
     'sports': 'Sport Page',
@@ -14,9 +14,18 @@ articles = {
 #     return HttpResponse(articles['finance'])
 
 def news_view(request, topic):
-    return HttpResponse(articles[topic])
+    try:
+        return HttpResponse(articles[topic])
+    except:
+        raise Http404()
 
 def add_view(request, num1, num2):
     #domain.com/my_app/num1/num2
     result = num1 + num2
     return HttpResponse(str(result))
+
+def num_page_view(request, num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+
+    return HttpResponseRedirect(f"/my_app/{topic}")
