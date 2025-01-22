f2b906bfb11658314d7a7ee53d6b2fcf29c4b691
kirill2ll <kirill.kvitkovski@gmail.com>
2025-01-22 11:38:44 +0200

    add tag if and else

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -8,5 +8,6 @@ def example_view(request):
 
 def variable_view(request):
     my_var = {'first_name': 'John', 'last_name': 'Doe',
-    'some_list': [1, 2, 3, 4, 5], 'some_dict': {'inside_key': 'inside_value'}}
+    'some_list': [1, 2, 3, 4, 5], 'some_dict': {'inside_key': 'inside_value'},
+    'user_logged_in': True}
     return render(request, "my_app/variable.html",context=my_var)
