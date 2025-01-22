c6dc4f5d1a6e1709466c967e08f59d7d3482e2ff
kirill2ll <kirill.kvitkovski@gmail.com>
2025-01-22 11:53:03 +0200

    add url names and links

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# register the app namespace, so Django can distinguish between different apps for URL NAMES
+app_name = "my_app"
+
 urlpatterns = [
     path("", views.example_view, name="example"),
     path("variable/", views.variable_view, name="variable"),
