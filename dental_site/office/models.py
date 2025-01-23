b7cc18412461540eaf87c0cccac75f7aa001d252
kirill2ll <kirill.kvitkovski@gmail.com>
2025-01-23 20:37:48 +0200

    default values and validators

--- dental_site/office/models.py
+++ dental_site/office/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Patient(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
-    
+    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(150)])
+    heart_rate = models.IntegerField(default=60)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.age}"
