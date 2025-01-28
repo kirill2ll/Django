deb95952b31557309772b922a26c9cf051cd623d
kirill2ll <kirill.kvitkovski@gmail.com>
2025-01-28 09:35:24 +0200

    add Car model and add/delete/list forms

--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse 
+from . import models
 
 # Create your views here.
 def list(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    return render(request, 'cars/list.html', context={'all_cars': all_cars})
 
 def add(request):
+    if request.method == 'POST':
+        brand = request.POST.get('brand')
+        year = int(request.POST.get('year'))
+        models.Car.objects.create(brand=brand, year=year)
+        return redirect(reverse('cars:list'))
+
     return render(request, 'cars/add.html')
 
 def delete(request):
+    if request.method == 'POST':
+        pk = request.POST.get('pk')
+        try:
+            car = models.Car.objects.get(pk=pk)
+            car.delete()
+        except:
+            print('Car not found')
+        return redirect(reverse('cars:list'))
     return render(request, 'cars/delete.html')
