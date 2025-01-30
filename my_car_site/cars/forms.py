977aee344d4312932489e7619073a190d95769df
kirill2ll <kirill.kvitkovski@gmail.com>
2025-01-30 13:24:58 +0200

    add widget styling from model

--- my_car_site/cars/forms.py
+++ my_car_site/cars/forms.py
@@ -4,4 +4,4 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=100)
     last_name = forms.CharField(label='Last Name', max_length=100)
     email = forms.EmailField(label='Email')
-    review = forms.CharField(label='Write your review here', widget=forms.Textarea)
+    review = forms.CharField(label='Write your review here', widget=forms.Textarea(attrs={'class':'myform','rows': 5, 'cols': 50}))
