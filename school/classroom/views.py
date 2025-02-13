1de251c713b7796b1d8f0ed735765675b97d40ef
kirill2ll <kirill.kvitkovski@gmail.com>
2025-02-13 12:22:46 +0200

    class based form views

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from .forms import ContactForm
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -7,4 +8,19 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html'
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    #success_url
+    success_url = '/classroom/thank_you/'
+    # success_url = reverse_lazy('classroom:thank_you')  #reverse_lazy is used to reverse the url
+
+    #what to do with the form
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
     
