876ee016e9d26050bf55248b60ae90555cff87f9
kirill2ll <kirill.kvitkovski@gmail.com>
2025-02-13 11:11:20 +0200

    class based views

--- school/classroom/urls.py
+++ school/classroom/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import HomeView, ThankYouView
 
 app_name = 'classroom'
 
 urlpatterns = [
-    path('', home_view, name='home'),   #path expects a function
+    path('', HomeView.as_view(), name='home'),   #path expects a function
+    path('thank_you/', ThankYouView.as_view(), name='thank_you'),  
 ]
