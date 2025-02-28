566f26cbc086206f889b867b3ff6c0c643c06ba6
kirill2ll <kirill.kvitkovski@gmail.com>
2025-02-28 13:23:23 +0200

    [ET:core/expenses] add styling, make login required

--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Expense
 
+@login_required
 def home(request):
     expenses = []
     if request.user.is_authenticated:
