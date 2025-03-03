da5b1d65c65e56ea88b46904d1b351afe201bf11
kirill2ll <kirill.kvitkovski@gmail.com>
2025-03-03 23:28:23 +0200

    wip

---
+++ expense_tracker/expenses/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Expense, Category
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['amount', 'date', 'description', 'category']
+        labels = {
+            'amount': 'Amount',
+            'date': 'Date',
+            'description': 'Description',
+            'category': 'Category'
+        }
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'})
+        }
+    
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].queryset = Category.objects.filter(user=user)
--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Expense
+from django.views.generic.edit import CreateView
 
 @login_required
 def home(request):
@@ -8,3 +9,14 @@ def home(request):
     if request.user.is_authenticated:
         expenses = Expense.objects.filter(user=request.user)
     return render(request, 'expenses/home.html', context={'expenses': expenses})
+
+class ExpenseCreateView(CreateView):
+    model = Expense
+    form_class = ExpenseForm
+    template_name = 'expenses/expense_form.html'
+    success_url = reverse_lazy('expenses:home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
